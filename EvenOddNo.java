a0d106f1c8ac5b96e9b635adb07e812d08142a9e
manasi025 <109495431+manasi025@users.noreply.github.com>
2022-08-16 02:59:46 +0530

    Update EvenOddNo.java

--- EvenOddNo.java
+++ EvenOddNo.java
@@ -19,3 +19,109 @@ public class EvenOddNo {
 		}
 	}
 }
+
+/*
+OUTPUT :
+
+Odd nos. are : 
+1
+Even nos. are : 
+2
+Odd nos. are : 
+3
+Even nos. are : 
+4
+Odd nos. are : 
+5
+Even nos. are : 
+6
+Odd nos. are : 
+7
+Even nos. are : 
+8
+Odd nos. are : 
+9
+Even nos. are : 
+10
+Odd nos. are : 
+11
+Even nos. are : 
+12
+Odd nos. are : 
+13
+Even nos. are : 
+14
+Odd nos. are : 
+15
+Even nos. are : 
+16
+Odd nos. are : 
+17
+Even nos. are : 
+18
+Odd nos. are : 
+19
+Even nos. are : 
+20
+Odd nos. are : 
+21
+Even nos. are : 
+22
+Odd nos. are : 
+23
+Even nos. are : 
+24
+Odd nos. are : 
+25
+Even nos. are : 
+26
+Odd nos. are : 
+27
+Even nos. are : 
+28
+Odd nos. are : 
+29
+Even nos. are : 
+30
+Odd nos. are : 
+31
+Even nos. are : 
+32
+Odd nos. are : 
+33
+Even nos. are : 
+34
+Odd nos. are : 
+35
+Even nos. are : 
+36
+Odd nos. are : 
+37
+Even nos. are : 
+38
+Odd nos. are : 
+39
+Even nos. are : 
+40
+Odd nos. are : 
+41
+Even nos. are : 
+42
+Odd nos. are : 
+43
+Even nos. are : 
+44
+Odd nos. are : 
+45
+Even nos. are : 
+46
+Odd nos. are : 
+47
+Even nos. are : 
+48
+Odd nos. are : 
+49
+Even nos. are : 
+50
+
+*/
