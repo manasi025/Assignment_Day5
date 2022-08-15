4341f81e6efd3b7150d872d285f4316a9e360a1b
manasi025 <109495431+manasi025@users.noreply.github.com>
2022-08-16 03:17:19 +0530

    Add files via upload

---
+++ VowelsConsonants.java
@@ -0,0 +1,20 @@
+package assignments;
+
+public class VowelsConsonants 
+{
+	public static void main(String[] args) 
+	{
+		
+		char ch = 'I';
+		
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+		{
+			System.out.println(ch + " is a Vowel");
+		}
+        else
+        {
+            System.out.println(ch + " is a Consonant");
+        }
+
+	}
+}
