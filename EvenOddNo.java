c786259a7cfb4d23c2948fb6921c9ba4aeb7e0c6
manasi025 <109495431+manasi025@users.noreply.github.com>
2022-08-16 12:48:47 +0530

    Update EvenOddNo.java

--- EvenOddNo.java
+++ EvenOddNo.java
@@ -1,127 +1,38 @@
 package assg;
 
-public class EvenOddNo {
+public class EvenOddNo 
+{
 	public static void main(String args[])
 	{
 		int n;
+		
+		System.out.print("Even nos. are : ");
 		for (n = 1; n <= 50; n++)
-		{
+		{	
 			if (n%2 == 0)
 			{
-				System.out.println("Even nos. are : ");
-				System.out.println(n);
+				System.out.print(n + "  ");
 			}
-			else
+	         }
+		System.out.print("\nOdd nos. are : ");
+		
+		for (n = 1; n <= 50; n++)
+		{
+			if (n%2 != 0)
+			
 			{
-				System.out.println("Odd nos. are : ");
-				System.out.println(n);
+				System.out.print(n + "  ");
 			}
+		
 		}
 	}
 }
 
+
 /*
 OUTPUT :
 
-Odd nos. are : 
-1
-Even nos. are : 
-2
-Odd nos. are : 
-3
-Even nos. are : 
-4
-Odd nos. are : 
-5
-Even nos. are : 
-6
-Odd nos. are : 
-7
-Even nos. are : 
-8
-Odd nos. are : 
-9
-Even nos. are : 
-10
-Odd nos. are : 
-11
-Even nos. are : 
-12
-Odd nos. are : 
-13
-Even nos. are : 
-14
-Odd nos. are : 
-15
-Even nos. are : 
-16
-Odd nos. are : 
-17
-Even nos. are : 
-18
-Odd nos. are : 
-19
-Even nos. are : 
-20
-Odd nos. are : 
-21
-Even nos. are : 
-22
-Odd nos. are : 
-23
-Even nos. are : 
-24
-Odd nos. are : 
-25
-Even nos. are : 
-26
-Odd nos. are : 
-27
-Even nos. are : 
-28
-Odd nos. are : 
-29
-Even nos. are : 
-30
-Odd nos. are : 
-31
-Even nos. are : 
-32
-Odd nos. are : 
-33
-Even nos. are : 
-34
-Odd nos. are : 
-35
-Even nos. are : 
-36
-Odd nos. are : 
-37
-Even nos. are : 
-38
-Odd nos. are : 
-39
-Even nos. are : 
-40
-Odd nos. are : 
-41
-Even nos. are : 
-42
-Odd nos. are : 
-43
-Even nos. are : 
-44
-Odd nos. are : 
-45
-Even nos. are : 
-46
-Odd nos. are : 
-47
-Even nos. are : 
-48
-Odd nos. are : 
-49
-Even nos. are : 
-50
+Even nos. are : 2  4  6  8  10  12  14  16  18  20  22  24  26  28  30  32  34  36  38  40  42  44  46  48  50  
+Odd nos. are : 1  3  5  7  9  11  13  15  17  19  21  23  25  27  29  31  33  35  37  39  41  43  45  47  49  
 
 */
